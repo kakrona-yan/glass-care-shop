d058402fd52dc0118dc0bef2e8cffc2a7ada78e8
kakrona <kakrona@marinax.co.jp>
2019-10-17 16:02:12 +0700

    add style in side bar

--- resources/views/backends/partials/footer.blade.php
+++ resources/views/backends/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-gray-light">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-        <span>រក្សាសិទ្ធិ &copy; ប្រព័ន្ធគ្រប់គ្រង​ ទំនិញ 2019</span>
+        <span>Copy right &copy; GLASS CARE SHOP 2019</span>
         </div>
     </div>
 </footer>
