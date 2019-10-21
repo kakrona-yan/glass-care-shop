2d47a86700771cbd998e0d9da70157aea082b57b
kakrona <kakrona@marinax.co.jp>
2019-10-21 10:03:05 +0700

    page contact

--- resources/views/frontends/pages/contact.blade.php
+++ resources/views/frontends/pages/contact.blade.php
@@ -18,4 +18,77 @@
         </nav>
     </div>
 </section>
+<section class="contact">
+    <div class="contact-link">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="contact--box">
+                        <div class="color-1">
+                            <i class="fas fa-map-marker-alt icon-bold"></i>
+                        </div>
+                        <div class="contact--text">
+                            <h1>Our Address</h1>
+                            <h2>str 200, Phnom Penh, Cambodia</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="contact--box">
+                        <div class="color-2">
+                            <i class="fas fa-phone-alt icon-bold"></i>
+                        </div>
+                        <div class="contact--text">
+                            <h1>Phone Number</h1>
+                            <h2>Office: (855) 123 456 789</h2>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="contact--box">
+                        <div class="color-3">
+                            <i class="fas fa-mail-bulk icon-bold"></i>
+                        </div>
+                        <div class="contact--text">
+                            <h1>Email Address</h1>
+                            <h2>galssCare168@gmail.com</h2>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="message">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                    <figure class="img-cavas">
+                        <a href="#"><img src="{{ URL('theme/img/550x450.png') }}" class="img-responsive" alt="img-holiwood"></a>
+                    </figure>
+                </div>
+                <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 message-contact">
+                    <h1>Send us a message</h1>
+                    <form class="form-group" action="" method="post">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label for="name-ip">Name<span>*</span></label><br>
+                            <input type="text" name="input-name" id="name-ip" class="input-lg form-control" placeholder="Mark Stevens">
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label for="mail-ip">Mail<span>*</span></label><br>
+                            <input type="text" name="input-mail" id="mail-ip" class="input-lg form-control" placeholder="Mark Stevens">
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label>What's on your mind?<span>*</span></label>
+                            <textarea placeholder="Write your message here..." class="form-control"></textarea>
+                        </div>
+                        <button type="submit">Send message</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <div id="map"></div> --}}
+</section>
 @endsection
