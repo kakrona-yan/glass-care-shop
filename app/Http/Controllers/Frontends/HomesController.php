ce43160d98a41b7cc631a4a380df878acd4e258d
kakrona yan <yan.kakrona199@gmail.com>
2019-10-24 23:17:26 +0700

    create new file

---
+++ app/Http/Controllers/Frontends/HomesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Frontends;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomesController extends Controller
+{
+   /**
+     * Show the application home.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function home()
+    {
+        return view('frontends.home');
+    }
+}
