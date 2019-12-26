5117aa62f2203f68fcbb7dfcbb5b2ff15920808d
kakrona <kakrona@marinax.co.jp>
2019-12-26 16:07:22 +0700

    update home page

--- resources/views/frontends/includes/menuDetail.blade.php
+++ resources/views/frontends/includes/menuDetail.blade.php
@@ -6,7 +6,7 @@
                     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 item-1">
                         <figure id="figure-show-1" class="item-show">
                             <a href="{{ route('about') }}">
-                                <img src="{{ URL('theme/img/890x490.png') }}" class="img-responsive" alt="{{ __('page.website') }}">
+                                <img src="{{ URL('theme/img/about.jpg') }}" class="img-responsive" alt="{{ __('page.website') }}">
                                 <div class="show-title">
                                     <h4>{{ __('page.website') }}</h4>
                                 </div>
@@ -16,7 +16,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 item-1">
                         <figure id="figure-show-2" class="item-show">
                             <a href="{{ route('collections.index') }}">
-                                <img src="{{ URL('theme/img/430x490.png') }}" class="img-responsive" alt="{{ __('page.collection') }}">
+                                <img src="{{ URL('theme/img/shop.jpg') }}" class="img-responsive" alt="{{ __('page.collection') }}">
                                 <div class="show-title">
                                     <h4>{{ __('page.collection') }}</h4>
                                 </div>
@@ -26,7 +26,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 item-1">
                         <figure id="figure-show-1" class="item-show">
                             <a href="{{ route('look') }}">
-                                <img src="{{ URL('theme/img/430x490.png') }}" class="img-responsive" alt="{{ __('page.look') }}">
+                                <img src="{{ URL('theme/img/look.jpg') }}" class="img-responsive" alt="{{ __('page.look') }}">
                                 <div class="show-title">
                                     <h4>{{ __('page.look') }}</h4>
                                 </div>
@@ -36,7 +36,7 @@
                     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 item-1">
                         <figure id="figure-show-2" class="item-show">
                             <a href="{{ route('shop') }}">
-                                <img src="{{ URL('theme/img/890x490.png') }}" class="img-responsive" alt="{{ __('page.shop') }}">
+                                <img src="{{ URL('theme/img/shop-place.jpg') }}" class="img-responsive" alt="{{ __('page.shop') }}">
                             </a>
                             <div class="show-title">
                                 <h4>{{ __('page.shop') }}</h4>
