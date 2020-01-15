f094e3ee6076fa8c999f796aabe2c8e195c7288f
kakrona <kakrona@marinax.co.jp>
2020-01-15 16:52:47 +0700

    change layout slider

--- resources/views/frontends/pages/news/include/slider.blade.php
+++ resources/views/frontends/pages/news/include/slider.blade.php
@@ -2,27 +2,28 @@
 <link rel="stylesheet" type="text/css" href="{{ URL('theme/slick/slick.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ URL('theme/slick/slick-theme.css') }}">   
 @endpush
-<div class="container">
+
 <div id="blog-slider" class="mb-50">
-    <div class="slider responsive-slick-banner">
-        @foreach ($blogSliders as $blogSlider)
-             <div class="slider-item">
-                <a href="{{ route('blog.detail', $blogSlider->permalink )}}">
-                    <img src="{{$blogSlider->thumbnail? asset(getUploadUrl($blogSlider->thumbnail, config('upload.news'))) : asset('images/no-thumbnail.jpg') }}" class="img-responsive" alt="{{$blogSlider->title}}">
-                </a>
-                <div class="blog-box">
-                    <div class="blog--title">
-                        <a href="{{ route('blog.detail', $blogSlider->permalink )}}">{{Str::limit($blogSlider->title, 45)}}</a>
-                    </div>
-                    <div class="blog--time">
-                        <span class="time"><i class="far fa-calendar-alt"></i><span> {{date('M, d Y', strtotime($blogSlider->created_at))}}</span></span>
-                        <span class="time"><i class="far fa-user"></i><span> {{$blogSlider->author}}</span></span>
+    <div class="container">
+        <div class="slider responsive-slick-banner">
+            @foreach ($blogSliders as $blogSlider)
+                <div class="slider-item">
+                    <a href="{{ route('blog.detail', $blogSlider->permalink )}}">
+                        <img src="{{$blogSlider->thumbnail? asset(getUploadUrl($blogSlider->thumbnail, config('upload.news'))) : asset('images/no-thumbnail.jpg') }}" class="img-responsive" alt="{{$blogSlider->title}}">
+                    </a>
+                    <div class="blog-box">
+                        <div class="blog--title">
+                            <a href="{{ route('blog.detail', $blogSlider->permalink )}}">{{Str::limit($blogSlider->title, 45)}}</a>
+                        </div>
+                        <div class="blog--time">
+                            <span class="time"><i class="far fa-calendar-alt"></i><span> {{date('M, d Y', strtotime($blogSlider->created_at))}}</span></span>
+                            <span class="time"><i class="far fa-user"></i><span> {{$blogSlider->author}}</span></span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
 </div>
 @push('footer-script')
 <script>
