0fe1ade0e636b8be1a6240236d9d421abf1195f3
kakrona <kakrona@marinax.co.jp>
2020-01-16 10:22:24 +0700

    update style blog

--- resources/views/frontends/includes/blog.blade.php
+++ resources/views/frontends/includes/blog.blade.php
@@ -3,18 +3,32 @@
         <h1 class="slider-title">Latest Blogs</h1>
         <div class="container">
             <div class="row">
+                @php
+                    $color = 1;    
+                @endphp
                 @foreach ($blogs as $blog)
                     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 product-blog mb-30">
-                    <a href="{{ route('blog.detail', $blog->permalink )}}">
-                        <img src="{{$blog->thumbnail? asset(getUploadUrl($blog->thumbnail, config('upload.news'))) : asset('images/no-thumbnail.jpg') }}" class="img-responsive" alt="{{$blog->title}}">
-                    </a>
-                    <div class="time-blog">
-                        <span class="time"><i class="far fa-calendar-alt"></i><span> {{date('M, d Y', strtotime($blog->created_at))}}</span></span>
-                        <span class="time"><i class="far fa-user"></i><span> {{$blog->author}}</span></span>
+                        <div class="product-blog-box">
+                            <a href="{{ route('blog.detail', $blog->permalink )}}">
+                                <div class="post-day {{$color > 3 || $color % 2 == 0 ? 'color-blue' : ''}}">
+                                    <div class="day">{{date('d', strtotime($blog->created_at))}}</div>
+                                    <div class="month">{{date('M', strtotime($blog->created_at))}}</div>
+                                </div>
+                                <img src="{{$blog->thumbnail? asset(getUploadUrl($blog->thumbnail, config('upload.news'))) : asset('images/no-thumbnail.jpg') }}" class="img-responsive" alt="{{$blog->title}}">
+                            </a>
+                            <div class="product-blog-detail">
+                                <div class="time-blog">
+                                    <span class="time"><i class="far fa-calendar-alt"></i><span> {{date('M, d Y', strtotime($blog->created_at))}}</span></span>
+                                    <span class="time"><i class="far fa-user"></i><span> {{$blog->author}}</span></span>
+                                </div>
+                                <h5><a href="{{ route('blog.detail', $blog->permalink )}}">{{Str::limit($blog->title, 45)}}</a></h5>
+                                <a href="{{ route('blog.detail', $blog->permalink )}}" class="read-more"><i class="fas fa-long-arrow-alt-right"></i> Read more</a>
+                            </div>
+                        </div>
                     </div>
-                    <h5><a href="{{ route('blog.detail', $blog->permalink )}}">{{Str::limit($blog->title, 45)}}</a></h5>
-                    <a href="{{ route('blog.detail', $blog->permalink )}}" class="read-more"><i class="fas fa-long-arrow-alt-right"></i> Read more</a>
-                </div>
+                    @php
+                        $color ++;
+                    @endphp
                 @endforeach
                
             </div>
