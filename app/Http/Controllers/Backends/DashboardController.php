a6bbcad25e845e188e59116cfd012b8c61e52d2f
kakrona <kakrona@marinax.co.jp>
2020-01-17 10:23:56 +0700

    update deshborad

--- app/Http/Controllers/Backends/DashboardController.php
+++ app/Http/Controllers/Backends/DashboardController.php
@@ -20,12 +20,22 @@ class DashboardController extends Controller
     public function index()
     {
         $productCount = $this->product->count();
+        $userCount = \App\Models\User::count();
+        $staffCount = \App\Models\Staff::count();
+        $customerCount = \App\Models\Customer::count();
+        $categoryCount = \App\Models\Category::count();
+        $newsCount = \App\Models\News::count();
         $products = $this->product->where('is_delete', '<>', DeleteStatus::DELETED)
             ->orderBy('id', 'desc')
-            ->paginate(config('pagination.limit'));
+            ->paginate(6);
         return view('backends.dashboard', [
             'productCount' => $productCount,
-            'products' => $products
+            'products' => $products,
+            'userCount' => $userCount,
+            'staffCount' => $staffCount,
+            'customerCount' => $customerCount,
+            'categoryCount' => $categoryCount,
+            'newsCount' => $newsCount
         ]);
 
     }
--- resources/views/backends/dashboard.blade.php
+++ resources/views/backends/dashboard.blade.php
@@ -11,42 +11,165 @@
     </div>
     <!-- Content Row -->
     <div class="row">
-        <!-- Earnings (Monthly) Card Example -->
+        <!-- Product -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                        <div class="fa-1x font-weight-bold text-primary text-uppercase mb-1"><a href="{{route('product.index')}}">Total Products</a></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$productCount}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="{{route('product.index')}}"><i class="fab fa-product-hunt fa-3x text-primary"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Products</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$productCount}}</div>
+                        <div class="fa-1x font-weight-bold text-danger text-uppercase mb-1"><a href="{{route('customer.index')}}">Total Customers</a></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$userCount}}</div>
                     </div>
                     <div class="col-auto">
-                    <i class="fab fa-product-hunt fa-2x text-gray-300"></i>
+                        <a href="{{route('customer.index')}}"><i class="fas fa-users fa-3x text-danger"></i></a>
                     </div>
                 </div>
+                </div>
             </div>
         </div>
-        </div>
-        <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-            <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Prices</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">0$</div>
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="fa-1x font-weight-bold text-info text-uppercase mb-1"><a href="{{route('customer.index')}}">Total Customers</a></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$customerCount}}</div>
+                    </div>
+                    <div class="col-auto">
+                        <a href="{{route('customer.index')}}"><i class="fas fa-user fa-3x text-info"></i></a>
+                    </div>
                 </div>
-                <div class="col-auto">
-                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
                 </div>
-            </div>
             </div>
         </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="fa-1x font-weight-bold text-warning text-uppercase mb-1"><a href="{{route('staff.index')}}">Total Staffs</a></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$staffCount}}</div>
+                    </div>
+                    <div class="col-auto">
+                        <a href="{{route('staff.index')}}"><i class="fas fa-users fa-3x text-warning"></i></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header text-white bg-danger">
+                    <div class="font-weight-bold">
+                        <span>Total Category</span>
+                    </div>
+                </div>
+                <div class="card-block py-0 px-4 b-t-1">
+                    <div class="row">
+                        <div class="col-4 b-r-1 py-3">
+                            <a href="{{route('category.index')}}"><i class="fas fa-tags fa-1x text-danger"></i></a>
+                        </div>
+                        <div class="col-8 py-3 text-right">
+                            <div class="font-weight-bold">{{$categoryCount}}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer px-3 py-2">
+                    <a class="font-weight-bold font-xs btn-block text-muted" href="{{route('category.index')}}">View More <i class="fa fa-angle-right float-right font-lg"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header text-white bg-warning">
+                    <div class="font-weight-bold">
+                        <span>Total News</span>
+                    </div>
+                </div>
+                <div class="card-block py-0 px-4 b-t-1">
+                    <div class="row">
+                        <div class="col-4 b-r-1 py-3">
+                            <a href="{{route('news.index')}}"><i class="fas fa-newspaper fa-1x text-warning"></i></a>
+                        </div>
+                        <div class="col-8 py-3 text-right">
+                            <div class="font-weight-bold">{{$newsCount}}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer px-3 py-2">
+                    <a class="font-weight-bold font-xs btn-block text-muted" href="{{route('news.index')}}">View More <i class="fa fa-angle-right float-right font-lg"></i></a>
+                </div>
+            </div>
+        </div>
+        <!-- Sale -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header text-white bg-success">
+                    <div class="font-weight-bold">
+                        <span>Total Sales</span>
+                    </div>
+                </div>
+                <div class="card-block py-0 px-4 b-t-1">
+                    <div class="row">
+                        <div class="col-4 b-r-1 py-3">
+                            <div class="font-weight-bold">Total</div>
+                        </div>
+                        <div class="col-8 py-3 text-right">
+                            <div class="font-weight-bold">0$</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer px-3 py-2">
+                    <a class="font-weight-bold font-xs btn-block text-muted" href="{{route('sale.index')}}">View More <i class="fa fa-angle-right float-right font-lg"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card">
+                <div class="card-header text-white bg-info">
+                    <div class="font-weight-bold">
+                        <span>Sales</span>
+                        <span class="label pull-right">This Month</span>
+                    </div>
+                </div>
+                <div class="card-block py-0 px-4 b-t-1">
+                    <div class="row">
+                        <div class="col-4 b-r-1 py-3">
+                            <div class="font-weight-bold">Total</div>
+                        </div>
+                        <div class="col-8 py-3 text-right">
+                            <div class="font-weight-bold">0$</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer px-3 py-2">
+                    <a class="font-weight-bold font-xs btn-block text-muted" href="{{route('sale.index')}}">View More <i class="fa fa-angle-right float-right font-lg"></i></a>
+                </div>
+            </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-xl-12 col-md-12 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+        <div class="col-12 col-md-7 mb-4">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Products</h6>
+                </div>
                 <div class="card-body">
                     <div class="table-responsive cus-table">
                     <table class="table table-striped table-bordered">
@@ -97,7 +220,20 @@
                 </div>
                 </div>
             </div>
-        </div>
+        </div><!--/col-12-->
+        <div class="col-12 col-md-5 mb-4">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Sales</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive cus-table">
+                  
+                    </div>
+                </div>
+            </div>
+        </div><!--/col-12-->
     </div>
 </div>
 @endsection
