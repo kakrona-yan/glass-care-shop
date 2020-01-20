0e894acc812cb745765b0e66db18fdae00b04079
kakrona <kakrona@marinax.co.jp>
2020-01-20 10:09:40 +0700

    add seo

--- resources/views/frontends/partials/head.blade.php
+++ resources/views/frontends/partials/head.blade.php
@@ -3,6 +3,13 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title')</title>
+<meta property="og:title" content="Swipe shop - Blog and News About Shop look contact us our place"/>
+<meta property="og:description" content="The Swipe shop blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, Glass screen protector premium,  Screen protector, News & Events, Tips & Guides, charging and the other gear you love!" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="The Swipe shop blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, charging and the other gear you love!" />
+<meta property="og:url" content="https://swipe-shop.com/" />
+<meta property="og:image" content="https://swipe-shop.com/theme/img/logo.png"/>
+<meta property="og:site_name" content="Swipe shop" />
 <link rel="icon" href="{{ URL('theme/img/favicon.png') }}" type="image/x-icon"/>
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
