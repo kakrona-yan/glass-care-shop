d85b585100a6b2d0ae8b838a2274d81ceac35314
kakrona <kakrona@marinax.co.jp>
2020-01-27 16:23:37 +0700

    update title og

--- resources/views/frontends/partials/head.blade.php
+++ resources/views/frontends/partials/head.blade.php
@@ -4,13 +4,13 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title')</title>
 <meta name="google-site-verification" content="nrO4-t83HKvA1vy5J8S4oyNvIZ-MUzu013Jp2SNUHT0" />
-<meta property="og:title" content="Swipe shop - Blog and News About Shop look contact us our place"/>
-<meta property="og:description" content="The Swipe shop blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, Glass screen protector premium,  Screen protector, News & Events, Tips & Guides, charging and the other gear you love!" />
+<meta property="og:title" content="Swipe & Shop & Blog and News About Shop look contact us our place"/>
+<meta property="og:description" content="Swipe & Shop & blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, Glass screen protector premium,  Screen protector, News & Events, Tips & Guides, charging and the other gear you love!" />
 <meta property="og:type" content="website" />
-<meta property="og:description" content="The Swipe shop blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, charging and the other gear you love!" />
+<meta property="og:description" content="The Swipe shop & blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, charging and the other gear you love!" />
 <meta property="og:url" content="https://swipe-shop.com/" />
 <meta property="og:image" content="https://swipe-shop.com/theme/img/logo.png"/>
-<meta property="og:site_name" content="Swipe shop" />
+<meta property="og:site_name" content="Swipe & Shop" />
 <link rel="icon" href="{{ URL('theme/img/favicon.png') }}" type="image/x-icon"/>
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
