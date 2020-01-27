59db669a60e36ef593913e1b633529ffeed0db72
kakrona <kakrona@marinax.co.jp>
2020-01-27 17:10:04 +0700

    change meta

--- resources/views/frontends/partials/head.blade.php
+++ resources/views/frontends/partials/head.blade.php
@@ -3,7 +3,7 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title')</title>
-<meta name="google-site-verification" content="nrO4-t83HKvA1vy5J8S4oyNvIZ-MUzu013Jp2SNUHT0" />
+<meta name="google-site-verification" content="_LpJNNvu5JnU2hACuLGALeiNwcJb79dUqejVDKkD6v0" />
 <meta property="og:title" content="Swipe & Shop & Blog and News About Shop look contact us our place"/>
 <meta property="og:description" content="Swipe & Shop & blog is your source for iPhone, iPad, AirPods, Mac, Samsung, Pixel and Apple Watch accessories buying guides. Don&#039;t miss out on product news, deals, reviews, tips or how-tos. Learn about the protective cases, screen protectors, Glass screen protector premium,  Screen protector, News & Events, Tips & Guides, charging and the other gear you love!" />
 <meta property="og:type" content="website" />
