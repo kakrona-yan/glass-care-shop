4363f188193b13a485a74878945e37acc2e96c63
kakrona <kakrona@marinax.co.jp>
2020-01-28 09:35:28 +0700

    add domain

--- resources/views/frontends/sitemap.blade.php
+++ resources/views/frontends/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($sitemapRoutes as $sitemapRoute)
     <url>
-        <loc>{{ $sitemapRoute['name'] }}</loc>
+        <loc>{{config('app.url')}}{{ $sitemapRoute['name'] }}</loc>
         <lastmod>{{ $sitemapRoute['date']->tz('Asia/Phnom_Penh')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
     </url>
