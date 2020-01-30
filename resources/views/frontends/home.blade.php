9ebdcc426f4ccab70c4c20943fbc0c1377771f40
kakrona <kakrona@marinax.co.jp>
2020-01-30 16:12:13 +0700

    add loading page

--- resources/views/frontends/home.blade.php
+++ resources/views/frontends/home.blade.php
@@ -7,6 +7,14 @@
 <link rel="stylesheet" type="text/css" href="{{ URL('theme/slick/slick-theme.css') }}">   
 @endpush
 @section('content')
+<div class="swipe-load">
+    <div class="loading-swipe">
+    <div class="item item-1"></div>
+    <div class="item item-2"></div>
+    <div class="item item-3"></div>
+    <div class="item item-4"></div>
+    </div>
+</div>
 <!--Slider-->
 @include('frontends.includes.slider')
 <!--Show Menu Detail-->
@@ -18,4 +26,12 @@
 @endsection
 @push('footer-script')
 <script src="{{ URL('theme/js/function-slick.js') }}"></script>
+<script>
+    $( window ).on( "load", function(){
+        setTimeout(function() {
+
+            $('.swipe-load').fadeOut(300);
+        }, 700);
+    });
+</script>
 @endpush
